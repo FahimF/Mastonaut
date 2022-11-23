dfc202c73925104c99b742bb640a1851ac3822d5
Fahim Farook <fahimf@gmail.com>
2022-11-23 11:29:22 +0400

    Working Logs & Sandboxing
    * Removed sandboxing and hardware hardening from project
    * Added code to create necessary folders and file for logging so that XCLogging can work

--- Shared/Logging.swift
+++ Shared/Logging.swift
@@ -11,7 +11,23 @@ import XCGLogger
 
 let log: XCGLogger = {
 	let log = XCGLogger(identifier: "advancedLogger")
-	let file = AutoRotatingFileDestination(writeToFile: "~/Documents/Mastonaut.log", identifier: "org.farook.Mastonaut", shouldAppend: true, maxTimeInterval: 86400)
+	let fm = FileManager.default
+	let dir = "/Users/Shared/Public/logs"
+	let path = "/Users/Shared/Public/logs/Mastonaut.log"
+	if !fm.fileExists(atPath: dir) {
+		do {
+			try fm.createDirectory(atPath: dir, withIntermediateDirectories: true)
+			if fm.createFile(atPath: path, contents: nil) {
+				NSLog("Created log file successfully")
+			} else {
+				NSLog("Failed to create log file")
+			}
+		} catch {
+			NSLog("Error creating folder path: \(error)")
+		}
+	}
+	let url = URL(fileURLWithPath: path)
+	let file = AutoRotatingFileDestination(writeToFile: url, identifier: "org.farook.Mastonaut", shouldAppend: true, maxTimeInterval: 86400)
 	// Customize as needed
 	file.showThreadName = true
 	file.logQueue = XCGLogger.logQueue
